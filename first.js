ca8a51fb7d4c9606627dae3563e1aafb63b707f7
Unnati897 <Manishaisha2002@gmail.com>
2024-07-19 21:40:35 +0530

    Initial commit

--- first.js
+++ first.js
@@ -1,1 +1,19 @@
-console.log("Unnati")
+//Airthmetic operators
+//let a = 5;
+//let b = 2;
+
+console.log("a= ", a, " & b =", 2);
+// console.log("a + b = ", a + b);
+// console.log("a - b ", a - b);
+// console.log("a * b ", a * b);
+// console.log("a / b ", a / b);
+// console.log("a % b ", a % b);
+// console.log("a ** b ", a ** b);
+
+//Unary Operator
+let a = 5;
+let b = 2;
+
+console.log("a= ", a, " & b =", 2);
+a = a + 1; //6
+console.log("a = ",a); //6
--- script.js
+++ script.js
@@ -1,5 +1,54 @@
+
 //Airthmetic operators
 let a = 5;
 let b = 2;
 
-console.log("a + b = ", a + b);
+console.log("a= ", a, " & b =", 2);
+// console.log("a + b = ", a + b);
+// console.log("a - b ", a - b);
+// console.log("a * b ", a * b);
+// console.log("a / b ", a / b);
+// console.log("a % b ", a % b);
+// console.log("a ** b ", a ** b);
+
+//Unary Operator
+let a = 5;
+let b = 2;
+
+console.log("a= ", a, " & b =", 2);
+a = a + 1; //6
+console.log("a = ", a); //6
+
+//Asignment operators
+let a = 5;
+let b = 2;
+
+a -=4; // a = a - 4
+console.log("a = ", a); //9
+
+//comparison operators
+let a = 5;
+let b = 2;
+
+console.log("6 >= 5", a >= b);
+
+//logical operators
+let a = 6;
+let b = 5;
+
+let cond1 = a > b; //true
+let cond2 = a ===6; //true
+console.log("cond1 && cond2 =", cond1 && cond2);
+
+
+
+
+
+
+
+
+
+
+
+
+
