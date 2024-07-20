bd83a729dda31f2c9a3cf23e367df442844a1a59
Unnati897 <Manishaisha2002@gmail.com>
2024-07-20 11:50:17 +0530

    Initial commit

--- script.js
+++ script.js
@@ -40,6 +40,49 @@ let cond1 = a > b; //true
 let cond2 = a ===6; //true
 console.log("cond1 && cond2 =", cond1 && cond2);
 
+//Exponential:-
+//let x=[1,2,3,4,5,6]
+//x[3]=3**2;
+//var y=0
+//for(i=0;i<x.length;i++){
+   // y=y+x[i]
+//}
+//console.log
+
+//In Array the number 2 how many times are
+repeated we are finding:-
+//let x=[2,3,5,2,8,5,9];
+//var y=0;
+// for(i=0;i<x.length;i++){
+    //  y++;
+    //}
+//{
+console.log("The number 2 Appears",y,"times in the array.");
+
+//creating variables:-
+// var y=20;
+//const z=9;
+//{
+   var x=10;
+//{
+//console.log(x);
+
+//Post increment :-
+/*let x=10;
+console.log("x",x)
+x++;
+console.log("x",x)*/
+
+
+
+
+
+
+
+
+
+
+
 
 
 
