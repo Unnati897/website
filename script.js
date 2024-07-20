e7570c7af329bc2d67fdefc2ed83b8bcd8f857b2
Unnati897 <Manishaisha2002@gmail.com>
2024-07-20 12:15:08 +0530

    Initial commit

--- script.js
+++ script.js
@@ -73,6 +73,26 @@ console.log("x",x)
 x++;
 console.log("x",x)*/
 
+//loops
+for(let count=1; count<=5; count++) {
+    console.log("Apna college");
+}
+
+//strings
+let str = "Apna college";
+
+console.log(str[0]); //A
+
+//Template literals
+console.log("Apna\n college");
+
+//functions
+function myfunction () {
+console.log("Hello!");
+console.log("I love javascript)");
+}
+
+
 
 
 
