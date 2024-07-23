6f26e3ca78afb022f41256daecb8dc0d7347cd79
Unnati897 <Manishaisha2002@gmail.com>
2024-07-23 21:05:39 +0530

    Initial commit

---
+++ bs.js
@@ -0,0 +1,21 @@
+//Break
+for(let i=0; i<5; i++)
+{
+    console.log(i);
+    if(i==3)
+    {
+        break;
+
+    }
+}
+
+//continue
+ for(i=1; i<5; i++)
+  {
+
+    if(i==3)
+    {
+        continue;
+    }
+    console.log(i);
+}
